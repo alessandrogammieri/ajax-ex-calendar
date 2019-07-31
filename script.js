561d18c4ff9140e67ffdbd59633c2a1f4e3256b0
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-31 20:22:58 +0200

    title january 2018

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ Il calendario partirà da gennaio 2018 e si concluderà a dicembre
 2018 (unici dati disponibili sull’API). */
 
 $( document ).ready(function () {
-  
+  // Dichiaro una variabile con giorno fisso 1 Gennaio 2018
+  var data = moment("2018-01-01");
+  // Stampo Gennaio 2018
+  $(".container h1").text(data.format("MMMM YYYY"));
 
 
 
