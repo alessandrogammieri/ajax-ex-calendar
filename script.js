3d3e27bbb6654e77fed18d6b5202d655e38b76d3
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-31 19:11:02 +0200

    initialized html + css + js

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Creare un calendario dinamico con le festività.
+Partiamo dal gennaio 2018 dando la possibilità di cambiare mese,
+gestendo il caso in cui l’API non possa ritornare festività.
+Il calendario partirà da gennaio 2018 e si concluderà a dicembre
+2018 (unici dati disponibili sull’API). */
+
+$( document ).ready(function () {
+
+  
+
+});
