11cca8b985ea5fb922c2a475a477c9d80040c18d
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-08-01 02:04:25 +0200

    january ok

--- script.js
+++ script.js
@@ -9,9 +9,21 @@ $( document ).ready(function () {
   var data = moment("2018-01-01");
   // Stampo Gennaio 2018
   $(".container h1").text(data.format("MMMM YYYY"));
+  // Creo il clone del template con Handlebars
+  var source = $("#template").html();
+  var template = Handlebars.compile(source);
+  // Dichiaro una variabile per il numero dei giorni in gennaio
+  var days = moment("2012-01").daysInMonth();
+  // Cicliamo per 31 volte
+  for (var i = 1; i <= days; i++) {
+    // Dichiaro un variabile con ogni giorno di Gennaio che cicla
+    var mese = moment("2018-01-01").day(i).format("ddd DD MMMM YYYY");
+    // Stampo l'input a schermo
+    var context = {giorno: mese};
+    $(".giorni ul").append(template(context));
+  }
 
-
-
+  
 
 
 
