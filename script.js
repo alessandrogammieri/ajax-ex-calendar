2221ec7719e3140e979204f2517732e223ec620e
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-08-01 11:40:07 +0200

    january holiday

--- script.js
+++ script.js
@@ -13,19 +13,41 @@ $( document ).ready(function () {
   var source = $("#template").html();
   var template = Handlebars.compile(source);
   // Dichiaro una variabile per il numero dei giorni in gennaio
-  var days = moment("2012-01").daysInMonth();
+  var days = moment("2018-01").daysInMonth();
   // Cicliamo per 31 volte
   for (var i = 1; i <= days; i++) {
     // Dichiaro un variabile con ogni giorno di Gennaio che cicla
-    var mese = moment("2018-01-01").day(i).format("ddd DD MMMM YYYY");
+    var mese = moment([2018, 0, i]).format("ddd DD MM YYYY");
     // Stampo l'input a schermo
-    var context = {giorno: mese};
+    var context = {giorno: mese, holi: moment([2018, 0, i]).format("YYYY-MM-DD")};
     $(".giorni ul").append(template(context));
   }
 
-  
-
-
-
+  // Richiediamo via ajax all'API le festività di Gennaio
+  $.ajax({
+    url : "https://flynn.boolean.careers/exercises/api/holidays",
+    method: "GET",
+    data: {"year": "2018", "month": 0},
+    success: function (data) {
+      if (data.success) {
+        // Dichiaro una variabile con i risultati della chiamata
+        var holiday = data.response;
+        // Cicliamo il nostro elenco delle festività
+        for (var i = 0; i < holiday.length; i++) {
+          // Creiamo una variabile per singola festività
+          var objholiday = holiday[i];
+          // Creiamo una variabile per identificare l'attributo nell'html
+          var hellfest = $("li[dateref='" + objholiday.date + "']");
+          if (hellfest) {
+            hellfest.append(", " + objholiday.name);
+            hellfest.addClass("orange");
+          }
+        }
+      }
+    },
+    error: function (errore) {
+      alert ("C'è stato un errore: " + errore);
+    }
+  });
 
 });
